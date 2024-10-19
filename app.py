86e5cb1c97f22cb19a8a749904957ff2e6cf926c
Dkkycorp <39125423+Dkkycorp@users.noreply.github.com>
2024-10-19 06:23:05 +0000

    1

--- app.py
+++ app.py
@@ -9,12 +9,8 @@ def index():
 
 @app.route("/prediciton_DBS",methods =["GET", 'POST'])
 def prediciton_DBS():
-    return(render_template("prediciton_DBS.html"))
-    #q = float(request.form.get("q"))
-    #return(render_template("prediciton_DBS.html" ,r=(-50.6*q)+90.2))
+    q = float(request.form.get("q"))
+    return(render_template("prediciton_DBS.html" ,r=(-50.6*q)+90.2))
 
 if __name__ == "__main__":
     app.run()
-
-
-    #testinggg
